87ced28dd02cb6b3b425b509756e67a080bcccf6
Oscar Rivera <oscar.rivera@alumnos.uneatlantico.es>
2026-02-17 12:51:07 +0000

    Eliminación de los comentarios

--- entregas/riveraOscar/src/reto-003/ExamenMina.java
+++ entregas/riveraOscar/src/reto-003/ExamenMina.java
@@ -2,40 +2,26 @@ import java.util.Scanner;
 
 public class ExamenMina{
 
-	static String[][] mapaMinasMostrar = 	
-		{ //[0][0]	[0][1]	[0][2]	[0][3]	[0][4]	[0][5]	[0][6]	[0][7]
+	static String[][] mapaMinasMostrar = {	
 			{" ",	"1", 	"2", 	"3", 	"4",	"5",	"6", 	"7"},
-		  //[1][0]	[1][1]	[1][2]	[1][3]	[1][4]	[1][5]	[1][6]	[1][7]
 			{"1", 	"-", 	"-", 	"-", 	"-", 	"-", 	"-",	"-"},
-		  //[2][0]	[2][1]	[2][2]	[2][3]	[2][4]	[2][5]	[2][6]	[2][7]
 			{"2", 	"-", 	"-",	"-", 	"-", 	"-", 	"-",	"-"},
-		  //[3][0]	[3][1]	[3][2]	[3][3]	[3][4]	[3][5]	[3][6]	[3][7]
 			{"3", 	"-", 	"-", 	"-",	"-", 	"-", 	"-",	"-"},
-		  //[4][0]	[4][1]	[4][2]	[4][3]	[4][4]	[4][5]	[4][6]	[4][7]
 			{"4", 	"-", 	"-",	"-", 	"-", 	"-", 	"-",	"-"},
-		  //[5][0]	[5][1]	[5][2]	[5][3]	[5][4]	[5][5]	[5][6]	[5][7]
 			{"5", 	"-", 	"-",	"-", 	"-", 	"-", 	"-",	"-"}
 		};
 		
-	static String[][] mapaMinasActivas = 	
-		{ //[0][0]	[0][1]	[0][2]	[0][3]	[0][4]	[0][5]	[0][6]	[0][7]
+	static String[][] mapaMinasActivas = {	
 			{" ",	" ", 	" ", 	" ", 	" ",	" ",	" ", 	" "},
-		  //[1][0]	[1][1]	[1][2]	[1][3]	[1][4]	[1][5]	[1][6]	[1][7]
 			{" ", 	"0", 	"0", 	"0", 	"0", 	"0", 	"0",	"0"},
-		  //[2][0]	[2][1]	[2][2]	[2][3]	[2][4]	[2][5]	[2][6]	[2][7]
 			{" ", 	"0", 	"0",	"0", 	"0", 	"0", 	"0",	"0"},
-		  //[3][0]	[3][1]	[3][2]	[3][3]	[3][4]	[3][5]	[3][6]	[3][7]
 			{" ", 	"0", 	"0", 	"0",	"0", 	"0", 	"0",	"0"},
-		  //[4][0]	[4][1]	[4][2]	[4][3]	[4][4]	[4][5]	[4][6]	[4][7]
 			{" ", 	"0", 	"0",	"0", 	"0", 	"0", 	"0",	"0"},
-		  //[5][0]	[5][1]	[5][2]	[5][3]	[5][4]	[5][5]	[5][6]	[5][7]
 			{" ", 	"0", 	"0",	"0", 	"0", 	"0", 	"0",	"0"}
 		};
 
-	//Posiciones Random de la mina
 	public static void main(String[] args){	
 	Scanner scan = new Scanner(System.in);
-		///////////////////////////
 		int cantidadMinas=0;	
 		while ( cantidadMinas < 5){
 			int PosYMina = (int)(Math.random()*2+1);
@@ -47,7 +33,6 @@ public class ExamenMina{
 		}
 	
 		int posX=0, posY=0, juegoEncendido=1, valoresCorrectos, contadorMapa=0, contadorMinas=0;
-		/////////////Comienza el juego
 		while(juegoEncendido==1){
 			for( int i = 0; i<mapaMinasMostrar.length; i++){            
 				for (int j = 0; j<=mapaMinasMostrar.length+1; j++) {                                     
@@ -55,7 +40,7 @@ public class ExamenMina{
 				}
 				System.out.println(" ");
 			}
-			valoresCorrectos=0;//reseteo de la variable
+			valoresCorrectos=0;
 			while(valoresCorrectos==0){
 				System.out.println(" ");
 				System.out.println("Ingrese X");
