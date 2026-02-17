4fb1326fa9c06bb254cde04b58f694de56089a93
Oscar Rivera <oscar.rivera@alumnos.uneatlantico.es>
2026-02-17 12:36:14 +0000

    Mejor implementación de if, simplificandolo y garantizando que cree las minas correctamente

--- entregas/riveraOscar/src/reto-003/ExamenMina.java
+++ entregas/riveraOscar/src/reto-003/ExamenMina.java
@@ -19,17 +19,17 @@ public class ExamenMina{
 		
 	static String[][] mapaMinasActivas = 	
 		{ //[0][0]	[0][1]	[0][2]	[0][3]	[0][4]	[0][5]	[0][6]	[0][7]
-			{" ",	"1", 	"2", 	"3", 	"4",	"5",	"6", 	"7"},
+			{" ",	" ", 	" ", 	" ", 	" ",	" ",	" ", 	" "},
 		  //[1][0]	[1][1]	[1][2]	[1][3]	[1][4]	[1][5]	[1][6]	[1][7]
-			{"1", 	"0", 	"0", 	"0", 	"0", 	"0", 	"0",	"0"},
+			{" ", 	"0", 	"0", 	"0", 	"0", 	"0", 	"0",	"0"},
 		  //[2][0]	[2][1]	[2][2]	[2][3]	[2][4]	[2][5]	[2][6]	[2][7]
-			{"2", 	"0", 	"0",	"0", 	"0", 	"0", 	"0",	"0"},
+			{" ", 	"0", 	"0",	"0", 	"0", 	"0", 	"0",	"0"},
 		  //[3][0]	[3][1]	[3][2]	[3][3]	[3][4]	[3][5]	[3][6]	[3][7]
-			{"3", 	"0", 	"0", 	"0",	"0", 	"0", 	"0",	"0"},
+			{" ", 	"0", 	"0", 	"0",	"0", 	"0", 	"0",	"0"},
 		  //[4][0]	[4][1]	[4][2]	[4][3]	[4][4]	[4][5]	[4][6]	[4][7]
-			{"4", 	"0", 	"0",	"0", 	"0", 	"0", 	"0",	"0"},
+			{" ", 	"0", 	"0",	"0", 	"0", 	"0", 	"0",	"0"},
 		  //[5][0]	[5][1]	[5][2]	[5][3]	[5][4]	[5][5]	[5][6]	[5][7]
-			{"5", 	"0", 	"0",	"0", 	"0", 	"0", 	"0",	"0"}
+			{" ", 	"0", 	"0",	"0", 	"0", 	"0", 	"0",	"0"}
 		};
 
 	//Posiciones Random de la mina
@@ -40,15 +40,10 @@ public class ExamenMina{
 		while ( cantidadMinas < 5){
 			int PosYMina = (int)(Math.random()*2+1);
 			int PosXMina = (int)(Math.random()*4+1);
-			if (mapaMinasActivas[PosXMina][PosYMina]=="1"){
-			cantidadMinas--;
-
-			}
-			else {
+			if (mapaMinasActivas[PosXMina][PosYMina]!="1"){
 				mapaMinasActivas[PosXMina][PosYMina]="1";
 				cantidadMinas++;
 			}
-			cantidadMinas++;
 		}
 	
 		int posX=0, posY=0, juegoEncendido=1, valoresCorrectos, contadorMapa=0, contadorMinas=0;
