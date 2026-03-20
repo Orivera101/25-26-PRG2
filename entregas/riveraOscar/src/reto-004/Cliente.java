7c8dd52ef7cb714d6888e71e0e32ec8c3cecfb9e
Oscar Rivera <oscar.rivera@alumnos.uneatlantico.es>
2026-03-20 00:17:57 +0000

    Prueba de suma de dos fracciones

--- entregas/riveraOscar/src/reto-004/Cliente.java
+++ entregas/riveraOscar/src/reto-004/Cliente.java
@@ -4,5 +4,9 @@ public class Cliente {
     public static void main(String[] args){
         Fraccion f1 = new Fraccion(1, 2);
         Fraccion f2 = new Fraccion(6, 8);
+
+        f1.mostrar();
+        f1.sumar(f2);
+        f1.mostrar();
     }
 }
