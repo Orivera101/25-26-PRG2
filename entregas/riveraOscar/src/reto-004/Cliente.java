92168023c05be6b6e6021873367d354b680d4fe6
Oscar Rivera <oscar.rivera@alumnos.uneatlantico.es>
2026-03-20 00:15:50 +0000

    Creación de objetos 1/2 y 6/8

--- entregas/riveraOscar/src/reto-004/Cliente.java
+++ entregas/riveraOscar/src/reto-004/Cliente.java
@@ -1,7 +1,8 @@
 package entregas.riveraOscar.src.Fraccion;
 
-public class Cliente(){
+public class Cliente {
     public static void main(String[] args){
-        
+        Fraccion f1 = new Fraccion(1, 2);
+        Fraccion f2 = new Fraccion(6, 8);
     }
 }
