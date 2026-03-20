6d22bd668b998020a6fef6d9fb2a2028822fa732
Oscar Rivera <oscar.rivera@alumnos.uneatlantico.es>
2026-03-20 00:23:13 +0000

    Prueba de metodos de comparación.

--- entregas/riveraOscar/src/reto-004/Cliente.java
+++ entregas/riveraOscar/src/reto-004/Cliente.java
@@ -2,11 +2,16 @@ package entregas.riveraOscar.src.Fraccion;
 
 public class Cliente {
     public static void main(String[] args){
+
         Fraccion f1 = new Fraccion(1, 2);
         Fraccion f2 = new Fraccion(6, 8);
 
         f1.mostrar();
         f1.sumar(f2);
         f1.mostrar();
+
+        Fraccion f3 = new Fraccion(5);
+        System.out.println("¿Es f3 mayor a f2? " + f3.esMayor(f2));
+        System.out.println("¿Es f3 igual a f1? " + f3.equals(f1));
     }
 }
