ef6411c81b0deaf2774b9c7a0687e9bdabc74df6
Oscar Rivera <oscar.rivera@alumnos.uneatlantico.es>
2026-04-26 21:53:29 +0000

    Vista privada e implementacion de metodos de la clase Caballo

--- entregas/riveraOscar/src/Caballo.java
+++ entregas/riveraOscar/src/Caballo.java
@@ -1,7 +1,19 @@
 public class Caballo {
-    public Caballo(int numero){}
+    private int numero;
+    private int posicion;
 
-    public void avanzar(){}
-    public int obtenerPosicion(){}
-    public int obtenerNumero(){}
+    public Caballo(int numero){
+        this.numero = numero;
+        this.posicion = 0;
+    }
+
+    public void avanzar(){
+        posicion = (int) (Math.random() * 3) + this.posicion;
+    }
+    public int obtenerPosicion(){
+        return this.posicion;
+    }
+    public int obtenerNumero(){
+        return this.numero;
+    }
 }
