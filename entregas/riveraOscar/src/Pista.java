c6c43b9916fd8320c7f9b5980894a9b4352c6433
Oscar Rivera <oscar.rivera@alumnos.uneatlantico.es>
2026-04-27 21:53:25 +0000

    Vista privada e implementación de métodos de clase Pista

--- entregas/riveraOscar/src/Pista.java
+++ entregas/riveraOscar/src/Pista.java
@@ -1,7 +1,48 @@
 public class Pista {
-    public Pista(){}
+    private int largoPista = 44;
+    private int distanciaMeta = 40;
+    private String dibujoCaballo = ";-;'";
+    private Console console;
 
-    public void mostrarPista(Caballo){}
-    public boolean existeGanador(){}
-    public boolean existeEmpate(){}
+    public Pista() {
+        this.console = new Console();
+    }
+
+    public void mostrarPista(Caballo[] caballos) {
+        for (int i = 0; i < largoPista; i++) {
+            console.write("-");
+        }
+        console.writeln("");
+
+        for (int i = 0; i < caballos.length; i++) {
+            for (int j = 0; j < caballos[i].obtenerPosicion(); j++) {
+                console.write(" ");
+            }
+            console.writeln(dibujoCaballo);
+        }
+
+        for (int i = 0; i < largoPista; i++) {
+            console.write("-");
+        }
+        console.writeln("");
+    }
+
+    public boolean existeGanador(Caballo[] caballos) {
+        for (Caballo caballo : caballos) {
+            if (caballo.obtenerPosicion() >= distanciaMeta) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean existeEmpate(Caballo[] caballos) {
+        int contadorGanadores = 0;
+        for (Caballo caballo : caballos) {
+            if (caballo.obtenerPosicion() >= distanciaMeta) {
+                contadorGanadores++;
+            }
+        }
+        return contadorGanadores > 1;
+    }
 }
