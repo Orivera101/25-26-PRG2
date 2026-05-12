0713448b891a59b57fbae19611cab84bb641c5d5
Oscar Rivera <oscar.rivera@alumnos.uneatlantico.es>
2026-05-12 02:05:47 +0000

    feat: vista privada de la clase Carta e implementacion de metodos

--- Carta.java
+++ Carta.java
@@ -1,6 +1,18 @@
 public class Carta{
-    public Carta(char valor, String palo){}
+    private String valor;
+    private String palo;
 
-    public int obtenerValor(){}
-    public void mostrarCarta(){}
+    public Carta(String valor, String palo){
+        this.valor = valor;
+        this.palo = palo;
+    }
+
+    public int obtenerValor(){
+        if (valor.equals("A")) return 11;
+        if (valor.equals("J") || valor.equals("Q") || valor.equals("K")) return 10;
+        return Integer.parseInt(valor);
+    }
+    public void mostrarCarta(){
+        System.out.print("[" + valor + " " + palo + "]"); 
+    }
 }
