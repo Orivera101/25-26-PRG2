ab5edcfeb06b178c6e3a214ae55fb191458a3151
Oscar Rivera <oscar.rivera@alumnos.uneatlantico.es>
2026-05-12 02:55:46 +0000

    feat: vista privada de clase Mano e implementacion de metodos de la misma clase

--- Mano.java
+++ Mano.java
@@ -1,7 +1,39 @@
 public class Mano {
-    public Mano(){}
+    private Carta[] misCartas;
+    int contadorMisCartas;
 
-    public void recibirCarta(Carta cartaActualCarta){}
-    public int calcularPuntaje(){}
-    public void mostrarMano(){}
+    public Mano(){
+        this.misCartas = new Carta[10]; 
+        this.contadorMisCartas = 0;
+    }
+
+    public void recibirCarta(Carta cartaActualCarta){
+        misCartas[contadorMisCartas] = cartaActualCarta;
+        contadorMisCartas++;
+    }
+    public int calcularPuntaje() {
+        int puntajeTotal = 0;
+        int cantidadAses = 0;
+
+        for (int i = 0; i < contadorMisCartas; i++) {
+            int valorCarta = misCartas[i].obtenerValor();
+            if (valorCarta == 11) {
+                cantidadAses++;
+            }
+            puntajeTotal += valorCarta;
+        }
+
+        while (puntajeTotal > 21 && cantidadAses > 0) {
+            puntajeTotal -= 10;
+            cantidadAses--;
+        }
+
+        return puntajeTotal;
+    }
+    public void mostrarMano() {
+        for (int i = 0; i < contadorMisCartas; i++) {
+            misCartas[i].mostrarCarta();
+        }
+        System.out.println(" - Puntaje: " + calcularPuntaje());
+    }
 }
